2a57cf798a6656491c21627e5a8ecec0e8bc3a92
itsherifAhmed <86168347+itsherifAhmed@users.noreply.github.com>
2022-01-15 22:01:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
-from random import randint
-for i in range (1,365):
-for j in range (0,randint(1,10)):
-d=str(i) + 'days ago' 
-with open ('file.txt','a') as file:
-file.write(d)
-os.system('git add .')
-os.system('git commit --date="' + d + '"-m "commit"')
-os.system('git push -u origin main)
+
+def makeCommits (days : int):
+    if days < 1:
+        os.system('git push')
+    else:
+        dates = f"{days} days ago"
+        with open('file.txt', 'a') as file:
+            file.write(f'{dates} <- this was the commit for the day!!\n')
+        
+        # staging 
+        os.system('git add file.txt')
+
+        # commit 
+        os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
+
+        return days * makeCommits(days - 1)
+
+makeCommits(300)
